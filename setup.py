38485477540aa7973bd52f7d00f5aa8903e808e2
Skymirrh <skymirrh@skymirrh.net>
2013-06-18 05:50:04 +0200

    Added MANIFEST.in and modified setup.py

--- setup.py
+++ setup.py
@@ -3,22 +3,23 @@ from distutils.core import setup
 
 def readme():
     """Read README file and return its contents"""
-    with open('README') as f:
+    with open('README.md') as f:
         return f.read()
 
 setup(
     name="namegenerator",
     description="Random name generator inspired by GitHub repository name suggestions",
     url="https://github.com/Skymirrh/glowing-hipster",
-    version="0.1",
+    version="0.2",
     author="Skymirrh",
     author_email="skymirrh@skymirrh.net",
     license="MIT",
     keywords="random name generator",
     packages=['namegenerator'],
+    package_data={'namegenerator': ['lists/*', 'lists_sizes']},
     long_description=readme(),
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: MIT License",
         "Topic :: Utilities",
         ],
